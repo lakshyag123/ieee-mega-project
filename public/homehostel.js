92177c7e779ea24da18dbb8a6c142fcc00d3a512
Lakshya Gupta <68389026+lakshyag123@users.noreply.github.com>
2020-11-23 21:22:30 +0530

    Add files via upload

---
+++ public/homehostel.js
@@ -0,0 +1,26 @@
+$(".btn").hover(function(){
+    $(this).css("background-color", "#21C9A6");
+    }, function(){
+    $(this).css("background-color", "#4c74b9");
+  });
+  $("#x").hover(function(){
+    $(this).css("background-color", "#1FBADE");
+    }, function(){
+    $(this).css("background-color", "#4c74b9");
+  });
+  $(".btn-squared-default").hover(function(){
+    $(this).css("background-color", "#32E08A");
+    }, function(){
+    $(this).css("background-color", "#4c74b9");
+  });
+  $(".but").hover(function(){
+    $(this).css("background-color","#1FBADE");
+    }, function(){
+    $(this).css("background-color", "white");
+  });
+  $(".btn-secondary").hover(function(){
+    $(this).css("background-color", "#1FBADE");
+    }, function(){
+    $(this).css("background-color", " #142034");
+  });
+//  1000px media query
